3d81a55040d5835010c3adb73a94a3f92b76928b
Algama17 <albertogarciamallada@gmail.com>
2025-02-10 21:50:03 +0100

    Implement IMC classification logic

--- src/main/java/dev/alberto/IMC.java
+++ src/main/java/dev/alberto/IMC.java
@@ -12,4 +12,23 @@ public class IMC {
         double imc = weight / Math.pow(height, 2);
         return Math.round(imc * 100.0) / 100.0;
     }
+    public static String classifyIMC(double imc) {
+        if (imc < 16) {
+            return "Delgadez severa";
+        } else if (imc >= 16 && imc < 17) {
+            return "Delgadez moderada";
+        } else if (imc >= 17 && imc < 18.5) {
+            return "Delgadez leve";
+        } else if (imc >= 18.5 && imc < 25) {
+            return "Peso normal";
+        } else if (imc >= 25 && imc < 30) {
+            return "Sobrepeso";
+        } else if (imc >= 30 && imc < 35) {
+            return "Obesidad leve";
+        } else if (imc >= 35 && imc < 40) {
+            return "Obesidad moderada";
+        } else {
+            return "Obesidad mórbida";
+        }
+    }
 }
