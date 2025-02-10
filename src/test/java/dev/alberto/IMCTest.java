77c61bc21a3310a2674393d9dd943efac76da694
Algama17 <albertogarciamallada@gmail.com>
2025-02-10 21:48:39 +0100

    Add tests for IMC classification

--- src/test/java/dev/alberto/IMCTest.java
+++ src/test/java/dev/alberto/IMCTest.java
@@ -13,7 +13,7 @@ public class IMCTest {
         assertEquals(19.53, IMC.calculatePeso(50, 1.60), 0.01);
         assertEquals(37.04, IMC.calculatePeso(120, 1.80), 0.01);
     }
-    Test
+    @Test
     public void testInvalidInputs() {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
             IMC.calculatePeso(70, -1.70);
@@ -24,5 +24,13 @@ public class IMCTest {
             IMC.calculatePeso(-70, 1.70);
         });
         assertEquals("El peso debe ser mayor que cero.", exception.getMessage());
+    }
+    @Test
+    public void testClassifyIMC() {
+        assertEquals("Delgadez severa", IMC.classifyIMC(15.99));
+        assertEquals("Delgadez moderada", IMC.classifyIMC(16.5));
+        assertEquals("Peso normal", IMC.classifyIMC(22.0));
+        assertEquals("Obesidad leve", IMC.classifyIMC(32.0));
+        assertEquals("Obesidad mórbida", IMC.classifyIMC(40.0));
     }
 }
