fa2bb6b065f5aa2f5733fdedefc4d87e24e6f2b6
Algama17 <albertogarciamallada@gmail.com>
2025-02-10 21:46:46 +0100

    Add tests for invalid input in IMC calculation

--- src/test/java/dev/alberto/IMCTest.java
+++ src/test/java/dev/alberto/IMCTest.java
@@ -1,6 +1,7 @@
 package dev.alberto;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,5 +12,17 @@ public class IMCTest {
         assertEquals(29.39, IMC.calculatePeso(90, 1.75), 0.01);
         assertEquals(19.53, IMC.calculatePeso(50, 1.60), 0.01);
         assertEquals(37.04, IMC.calculatePeso(120, 1.80), 0.01);
+    }
+    Test
+    public void testInvalidInputs() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            IMC.calculatePeso(70, -1.70);
+        });
+        assertEquals("La altura debe ser mayor que cero.", exception.getMessage());
+
+        exception = assertThrows(IllegalArgumentException.class, () -> {
+            IMC.calculatePeso(-70, 1.70);
+        });
+        assertEquals("El peso debe ser mayor que cero.", exception.getMessage());
     }
 }
